e4fb49ad8faa2ece2cd2c9c0021cf7c22d80a9a1
JimmyHaviland <wjhcp6@mst.edu>
2016-11-05 22:53:20 -0500

    created basic class for i2c. ATM just has send functions
    basic class for single master i2c communication, will continue to
    implement send/receive functions for sending/receiving one or multiple
    bytes data to and from slave.

---
+++ tests/i2cTester/i2cComm.h
@@ -0,0 +1,40 @@
+#define PART_TM4C1294NCPDT //the part to use, replace if you're using a different part
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "driverlib/pin_map.h"
+#include "inc/hw_i2c.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/i2c.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+class I2CComm
+{
+  private:
+    uint32_t i2cBase;
+    
+  public:
+    bool init(uint8_t PinSCL, uint8_t PinSDA);
+    void send(uint8_t SlaveAddr, uint8_t msg);
+    void send(uint8_t SlaveAddr, uint8_t msg[]);
+    uint8_t receive(uint8_t Addr);
+    uint8_t* receive(uint8_t Addr, uint32_t sizeOfReceive);
+    I2CComm();
+    ~I2CComm();
+};
+
+
+
+
+
+
+
+
+
+
+
+
