2e0da94f98e80bbe340d944cb0c511185cb2b207
Drue Satterfield <drscp5@mst.edu>
2017-04-17 19:54:45 -0500

    Removed deprecated output devices

--- tests/OpenLTest/OpenLTest.ino
+++ tests/OpenLTest/OpenLTest.ino
@@ -1,4 +1,5 @@
 #include "JointControlFramework.h"
+#include "DRV8388.h"
 
 /* Programmers: David strickland, Jake Hasenfratz, Drue Satterfield
  * Date of creation: 2/5/17
@@ -11,7 +12,12 @@
 JointInterface* J1_tilt;
 JointInterface* J2_rot;
 JointInterface* Gripper;
-
+JointInterface* genJoint;
+JointInterface* genJoint2;
+JointInterface* genJoint3;
+JointInterface* genJoint4;
+JointInterface* genJoint5;
+IOAlgorithm* alg;
 void setup() {} //fuck you setup
 
 void loop() {
@@ -29,7 +35,11 @@ void loop() {
     //J2_rot->runOutputControl(1000);
     //delay(4000);
 
-    Gripper->runOutputControl(-1000);
+    genJoint->runOutputControl(-1000);
+    genJoint2->runOutputControl(1000);
+    genJoint3->runOutputControl(1000);
+    genJoint4->runOutputControl(1000);
+    genJoint5->runOutputControl(1000);
   }
    
   
@@ -37,19 +47,27 @@ void loop() {
 
 void initialize()
 {
- 
   pinMode(PA_7, OUTPUT);
   pinMode(PL_2, OUTPUT);
   pinMode(PE_4, OUTPUT);
   pinMode(PP_3, OUTPUT);
+  pinMode(PH_1, OUTPUT);
+  pinMode(PD_5, OUTPUT);
+  pinMode(PK_3, OUTPUT);
 
+  digitalWrite(PD_5, HIGH);
+  digitalWrite(PH_1, HIGH);
   digitalWrite(PA_7, HIGH);
   digitalWrite(PL_2, HIGH);
   digitalWrite(PE_4, HIGH);
+  digitalWrite(PK_3, HIGH);
   digitalWrite(PP_3, LOW);
   
   OutputDevice* controller1 = new DRV8388(PG_1, PP_5, false);
   OutputDevice* controller2 = new DRV8388(PF_3, PL_3, true);
+  OutputDevice* controller3 = new DRV8388(PK_5, PK_6, true);
+  OutputDevice* controller4 = new DRV8388(PK_4, PA_5, true);
+  OutputDevice* controller5 = new DRV8388(PG_0, PQ_0, true);
   OutputDevice* GripMot = new DRV8388(PF_2, PQ_2, true);
 
   J1_tilt = new TiltJoint(spd, controller2, controller1);
@@ -58,6 +76,12 @@ void initialize()
   J1_tilt->coupleJoint(J2_rot);
 
   Gripper = new SingleMotorJoint(spd, GripMot);
+
+  genJoint = new SingleMotorJoint(spd, controller1);
+  genJoint2 = new SingleMotorJoint(spd, controller2);
+  genJoint3 = new SingleMotorJoint(spd, controller3);
+  genJoint4 = new SingleMotorJoint(spd, controller4);
+  genJoint5 = new SingleMotorJoint(spd, controller5);
 }
 
 
