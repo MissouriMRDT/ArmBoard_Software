2a6976a7db270eec5995458cce96b8d2480951eb
Drue Satterfield <drscp5@mst.edu>
2017-05-04 20:45:04 -0500

    added ability to create angular offset in ma3Encoder

--- libraries/JointControlFramework/Ma3Encoder12b.cpp
+++ libraries/JointControlFramework/Ma3Encoder12b.cpp
@@ -7,7 +7,10 @@ long Ma3Encoder12b::getFeedback()
   
   //values will be between PWM_READ_MIN and PWM_READ_MAX, that is 1 and 4097. 
   //Or at least they should be; if it's above there was slight comm error and it can be scaled down to the max val.
-  if(readOnPeriod > PWM_READ_MAX) readOnPeriod = PWM_READ_MAX;
+  if(readOnPeriod > PWM_READ_MAX)
+  {
+    readOnPeriod = PWM_READ_MAX;
+  }
 
   //Alternatively, if the value read is 0 then it means the duty cycle is either at 0 or 100%, 
   //so if we get a 0 then we need to check the duty cycle to see which it is.
@@ -15,14 +18,31 @@ long Ma3Encoder12b::getFeedback()
   if(readOnPeriod == 0)
   {
     if(getDuty(pwmMappedPin) == 0)
+    {
       readOnPeriod = PWM_READ_MIN;
+    }
     else
+    {
       readOnPeriod = PWM_READ_MAX;
+    }
   }
-  //scale the values from the pwm values to the common position values, IE 1-4097 to POS_MIN-POS_MAX
-  return map(readOnPeriod, PWM_READ_MIN, PWM_READ_MAX, POS_MIN, POS_MAX);
+  
+  //scale the values from the pwm values to the common position values, IE 1-4097 to POS_MIN-POS_MAX, giving the absolute read angle
+  long absoluteAngle = map(readOnPeriod, PWM_READ_MIN, PWM_READ_MAX, POS_MIN, POS_MAX);
+  
+  //set the relative angle, and account for any overflow due to the calculations
+  long relativeAngle = absoluteAngle+offsetAngle;
+  if(relativeAngle > POS_MAX)
+  {
+    relativeAngle -= (POS_MAX-POS_MIN); 
+  }
+  else if(relativeAngle < POS_MIN)
+  {
+    relativeAngle += (POS_MAX-POS_MIN);
+  }
+  
+  return(relativeAngle);
 }
-
 
 Ma3Encoder12b::Ma3Encoder12b(uint8_t mappedPinNumber): FeedbackDevice()
 {
@@ -30,4 +50,16 @@ Ma3Encoder12b::Ma3Encoder12b(uint8_t mappedPinNumber): FeedbackDevice()
   fType = pos;
   
   initPwmRead(pwmMappedPin); 
+}
+
+float Ma3Encoder12b::getFeedbackDegrees()
+{
+  float position = getFeedback();
+  return(map(position, (float)POS_MIN, (float)POS_MAX, 0.0, 360.0));
+}
+
+void Ma3Encoder12b::setOffsetAngle(float offset)
+{
+  offset = constrain(offset, 0.0, 360.0); //constrain offset to 0-360 degrees
+  offsetAngle = map(offset, 0.0, 360.0, POS_MIN, POS_MAX) ; //offset in 0-360, so convert to framework positional values
 }
--- libraries/JointControlFramework/Ma3Encoder12b.h
+++ libraries/JointControlFramework/Ma3Encoder12b.h
@@ -10,6 +10,7 @@ class Ma3Encoder12b: public FeedbackDevice
     uint8_t pwmMappedPin;
     const int PWM_READ_MAX = 4097;
     const int PWM_READ_MIN = 1;
+    long offsetAngle = 0;
 
   public:
 
@@ -22,6 +23,15 @@ class Ma3Encoder12b: public FeedbackDevice
     //Note that the feedback can fluctuate a bit due to encoder's natural error tolerance. If unreliable results are returned consistently,
     //taking an average of returned values might work in your favor
     long getFeedback();
+    
+    //sets an angular offset. For example, if you want absolute angle 37 to represent angle 0 for the framework's calculations, 
+    //enter -37. The offset gets added to the absolute read angle, and the new relative angle is what the rest of the framework shall consider
+    //to be the joints 'true' angle
+    //input: Offset angle, expressed in 0-360 degrees
+    void setOffsetAngle(float offset);
+    
+    //same as getFeedback, but returns 0-360 degrees
+    float getFeedbackDegrees();
 };
 
 #endif
