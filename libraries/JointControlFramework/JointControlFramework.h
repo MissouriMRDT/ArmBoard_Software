6cd3f4f4d1c4136f7269d2a765a089c7a92a1268
Drue Satterfield <drscp5@mst.edu>
2017-07-06 14:18:49 -0500

    forgot this file during merge

--- libraries/JointControlFramework/JointControlFramework.h
+++ libraries/JointControlFramework/JointControlFramework.h
@@ -1,44 +1,7 @@
-/*  Programmer: Drue Satterfield, David Strickland
- *
- *   This is the library for the Joint Interface Framework.
- *   Framework description here: https://github.com/MST-MRDT/ArmBoardSoftware/blob/development/libraries/ControlFramework/Framework%20Overview.docx
- *   Used for controlling output devices and easily manipulating joints. The user will only ever interact with the
- *   Joint Interface, which keeps track of an output device which moves that joint, and an algorithm for controlling that joint.
- *
- *   necessary libraries:
- *   This program is meant to use energia libraries as well as the RoveDynamixel library and the PwmReader library and the PwmWriter library
- *
- *   implemenation notes:
- *   The user is supposed to use the library in this fashion:
- *   1) construct the output device class/classes representing the output device/devices (a motor controller, an h bridge, etc) used to move the joint
- *   2) if the joint is closed loop controlled, then the user must also construct a feedback device class representing the feedback device equipped on the joint as well as
- *      construct the IOAlgorithm class representing the closed loop algorithm they wish to use to control the joint
- *   3) Finally, construct the JointInterface class that represents this joint by passing it in the output device/devices and the constructed feedback device and closed loop algorithm, if used
- *      On top of those, the constructor also will have the user specify what kind of values they will pass the joint interface class, such as positional values or speed values. This is so
- *      that the interface knows how to properly interpret commands
- *   4) From here on out, the user should be able to simply call on the joint interface class for that joint to control the joint.
- *   note) When I say joint interface class/IOAlgorithm/output device/feedback device, I mean their specific derived classes representing the specific thing used on the joint, the formers are
- *         all abstract superclasses.
- *
- *   If the joint is open loop controlled -- IE there is no feedback device -- then  user has no control on the selected algorithm, the joint interface constructor selects an open loop
- *   algorithm internally since open loop algorithms aren't particularly complex or in need of user input.
- *
- *   In order to implement modules into the program:
- *   Inherit from the proper abstract class.
- *   If you are implementing an algorithm, and that algorithm doens't use feedback, then
- *   go down to Joint Interface's selector method and put in the logic to select your new algorithm accordingly.
- *   Don't forget to make a constructor; if it's a device class then it should take in whatever parameters it needs to output properly such as hardware pins being used.
- *   If it's an open loop algorithm class, then the constructor likely doesn't take anything and all it needs to do internally is set the algorithm's input and output types.
- *   Also for open loop algorithm classes, their class setup must specify that JointInterface is a friend class (as the constructor should be protected since the user never needs it)
- *   If it's a closed loop algorithm class, then the constructor should take in any parameters used to configure the algorithm as well as internally set the input and output types.
- *   For closed loop algorithm classes, the constructor should be public since the user will need to construct them personally
- *
- *   Once you do implement a new module, make sure to record it in the modules in the framework text file that should be in the framework libarary's directory
-*/
-
 #ifndef JOINTCONTROLFRAMEWORK_H_
 #define JOINTCONTROLFRAMEWORK_H_
 
+<<<<<<< HEAD
 #include "Energia.h"
 #include <RoveDynamixel.h>
 #include <PwmReader.h>
@@ -626,12 +589,14 @@ class Ma3Encoder12b: public FeedbackDevice
       pwmMappedPin = mappedPinNumber;
       fType = pos;
     }
+=======
+#include "JointFrameworkUtilities.h"
+#include "AbstractFramework.h"
+>>>>>>> refs/remotes/origin/Library_Module_dev
 
-    //gets the positional feedback from the encoder. Returns positional values from POS_MIN and POS_MAX.
-    //Note that the feedback can fluctuate a bit due to encoder's natural error tolerance. If unreliable results are returned consistently,
-    //taking an average of returned values might work in your favor
-    long getFeedback();
-};
+#include "TiltJoint.h"
+#include "SingleMotorJoint.h"
+#include "RotateJoint.h"
 
 //feedback device for the MA3 encoder, 10 bit version, using the Energia AnalogRead function
 //note: not the PWM read function. analog works in 10 bit mode.
