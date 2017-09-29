f1648212267a10a78c3bd90efe490eff83aa5a87
M20Dragon <hgtimur@hotmail.com>
2017-09-29 00:16:58 -0500

    Completed compiling version of PIVConverter

--- RoveJointControl/PIVConverter.h
+++ RoveJointControl/PIVConverter.h
@@ -22,19 +22,24 @@ class PIVConverter : public DrivingAlgorithm
     bool validConstruction;
 
     //pointer to the feedback device used by this algorithm
-    FeedbackDevice * feedbackDev;
+    FeedbackDevice * feedbackDevVelocity;
+    FeedbackDevice * feedbackDevPosition;
 
     float deg_deadBand;//when the joint is within this many degrees of its destination, it stops
 
     //Ki and Kp are PI loop values needed to calculate the output.
-    int KI, KP;
+    int KPP, KIP, KPV, KIV;
+
+    //Represents how many cycles are left before the timer resets
+    int posCyclesLeft;
+
+     //Represents the amount of cycles the timer will run before the timer resets
+     int posReloadCycles;
 
     //dT represents the time slice constant in seconds for this control loop. It should be externally looped, with dt representing
     //how many seconds pass in between calls to the control loop.
-    //ErrorSummation keeps track of how large our previous errors were when trying to get to the desired destination, used to
-    //calculate power output in the PI loop
     //hardStops one and two track physical stops that the joint can't move through like walls. Initialized to -1 as a value representing they're unassigned
-    float DT, errorSummation, hardStopPos1, hardStopPos2;
+    float DT, errorVelSummation, errorPosSummation, hardStopPos1, hardStopPos2;
 
     //Function that converts rotation units into something that can be worked with more easily such as degrees.
     float dist360(int pos_ru);
@@ -53,22 +58,29 @@ class PIVConverter : public DrivingAlgorithm
     // Upon being called, the method will run PI logic on the passed input and return a value of power for how fast
     // the motor controlling this joint should move
     // If ret_OutputFinished returns false but input returns 0, it's an indication that an error has occured
-    long runAlgorithm(const long input, bool * ret_OutputFinished);
 
+
+    //long runAlgorithm(const long input, bool * ret_OutputFinished, bool calcPos);
+    long runPosAlgorithm(const long posDest, float *deg_disToDest);
+    int runVelAlgorithm(int speedDest, int *speedError);
+    long runAlgorithm(const long input, bool * ret_OutputFinished);
 
 
     public:
 
 
 
-    PIVConverter(int inKP, int inKI, float inDT, FeedbackDevice* fDev);
+    PIVConverter(uint32_t inKPP, uint32_t inKIP, uint32_t inKPV, uint32_t inKIV, float inDT, FeedbackDevice* posFeed, FeedbackDevice* velFeed);
 
     //function for specifying positions of hard stops attached to this joint, that is positions in degrees that the joint can't travel through
     //To disable hard stops, set one or both to -1.
     void setHardStopPositions(float hardStopPos1_deg, float hardStopPos2_deg);
 
     //sets the deadband for the PI algorithm, in degrees. When it gets within this many degrees of its destination it stops.
-    void setDeadband(float degrees);
+    void setPositionDeadband(float degrees);
+
+    //PENDING DANK COMMENT
+    void setPosValLoopRatio(int ratio);
 
 };
 
