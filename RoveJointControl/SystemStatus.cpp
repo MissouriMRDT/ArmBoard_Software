f798d3144898524edadd414fef150a599f0f72a1
cathyrae903 <cathyrae903@gmail.com>
2017-09-30 20:14:31 -0500

    Created SystemStatus class without math implementation.

---
+++ RoveJointControl/SystemStatus.cpp
@@ -0,0 +1,74 @@
+
+#include "SystemStatus.h"
+#include "RoveBoard.h"
+
+SystemStatus::SystemStatus(const double j1CenterOfGravity, const double j1Weight, const double j1Length,
+                           const double j2CenterOfGravity, const double j2Weight, const double j2Length,
+                           const double j3CenterOfGravity, const double j3Weight, const double j3Length,
+                           const double j4CenterOfGravity, const double j4Weight, const double j4Length,
+                           const double j5CenterOfGravity, const double j5Weight, const double j5Length,
+                           const double j6CenterOfGravity, const double j6Weight, const double j6Length) :
+                           J1_CENTER_OF_GRAVITY(j1CenterOfGravity), J1_WEIGHT(j1Weight), J1_LENGTH(j1Length),
+                           J2_CENTER_OF_GRAVITY(j2CenterOfGravity), J2_WEIGHT(j2Weight), J2_LENGTH(j2Length),
+                           J3_CENTER_OF_GRAVITY(j3CenterOfGravity), J3_WEIGHT(j3Weight), J3_LENGTH(j3Length),
+                           J4_CENTER_OF_GRAVITY(j4CenterOfGravity), J4_WEIGHT(j4Weight), J4_LENGTH(j4Length),
+                           J5_CENTER_OF_GRAVITY(j5CenterOfGravity), J5_WEIGHT(j5Weight), J5_LENGTH(j5Length),
+                           J6_CENTER_OF_GRAVITY(j6CenterOfGravity), J6_WEIGHT(j6Weight), J6_LENGTH(j6Length)
+{
+}
+
+// Empty because there are no pointers.
+SystemStatus::~SystemStatus()
+{
+}
+
+void SystemStatus::update()
+{
+    // Math here.
+}
+
+double SystemStatus::getGravity(uint32_t id)
+{
+    // Return the appropriate value based on the joint.
+    // id 1 corresponds to joint 1, id 2 corresponds to joint 2, etc.
+    switch (id)
+    {
+    case 1:
+        return j1Gravity;
+    case 2:
+        return j2Gravity;
+    case 3:
+        return j3Gravity;
+    case 4:
+        return j4Gravity;
+    case 5:
+        return j5Gravity;
+    case 6:
+        return j6Gravity;
+    default:
+        return 0.0;
+    }
+}
+
+double SystemStatus::getInertia(uint32_t id)
+{
+    // Return the appropriate value based on the joint.
+    // id 1 corresponds to joint 1, id 2 corresponds to joint 2, etc.
+    switch (id)
+    {
+    case 1:
+        return j1Inertia;
+    case 2:
+        return j2Inertia;
+    case 3:
+        return j3Inertia;
+    case 4:
+        return j4Inertia;
+    case 5:
+        return j5Inertia;
+    case 6:
+        return j6Inertia;
+    default:
+        return 0.0;
+    }
+}
---
+++ RoveJointControl/SystemStatus.h
@@ -0,0 +1,97 @@
+/*
+ * SystemStatus.h
+ *
+ * Calculates the Gravity torque and inertia of the arm with values from all joints.
+ */
+
+#ifndef ROVEJOINTCONTROL_ARMBOARDSOFTWARE_ROVEJOINTCONTROL_SYSTEMSTATUS_H_
+#define ROVEJOINTCONTROL_ARMBOARDSOFTWARE_ROVEJOINTCONTROL_SYSTEMSTATUS_H_
+
+#include "AbstractFramework.h"
+
+class SystemStatus
+{
+private:
+    // Constants.
+    const double J1_CENTER_OF_GRAVITY;
+    const double J1_WEIGHT;
+    const double J1_LENGTH;
+    const double J2_CENTER_OF_GRAVITY;
+    const double J2_WEIGHT;
+    const double J2_LENGTH;
+    const double J3_CENTER_OF_GRAVITY;
+    const double J3_WEIGHT;
+    const double J3_LENGTH;
+    const double J4_CENTER_OF_GRAVITY;
+    const double J4_WEIGHT;
+    const double J4_LENGTH;
+    const double J5_CENTER_OF_GRAVITY;
+    const double J5_WEIGHT;
+    const double J5_LENGTH;
+    const double J6_CENTER_OF_GRAVITY;
+    const double J6_WEIGHT;
+    const double J6_LENGTH;
+
+    // Member variables.
+    double j1Gravity;
+    double j1Inertia;
+    double j2Gravity;
+    double j2Inertia;
+    double j3Gravity;
+    double j3Inertia;
+    double j4Gravity;
+    double j4Inertia;
+    double j5Gravity;
+    double j5Inertia;
+    double j6Gravity;
+    double j6Inertia;
+
+protected:
+
+public:
+    // Description:     Constructor
+    // Arguments:       j1CenterOfGravity - Center of gravity for joint 1.
+    //                  j1Weight          - Weight for joint 1.
+    //                  j1Length          - Length for joint 1.
+    //                  j2CenterOfGravity - Center of gravity for joint 2.
+    //                  j2Weight          - Weight for joint 2.
+    //                  j2Length          - Length for joint 2.
+    //                  j3CenterOfGravity - Center of gravity for joint 3.
+    //                  j3Weight          - Weight for joint 3.
+    //                  j3Length          - Length for joint 3.
+    //                  j4CenterOfGravity - Center of gravity for joint 4.
+    //                  j4Weight          - Weight for joint 4.
+    //                  j4Length          - Length for joint 4.
+    //                  j5CenterOfGravity - Center of gravity for joint 5.
+    //                  j5Weight          - Weight for joint 5.
+    //                  j5Length          - Length for joint 5.
+    //                  j6CenterOfGravity - Center of gravity for joint 6.
+    //                  j6Weight          - Weight for joint 6.
+    //                  j6Length          - Length for joint 6.
+    // Derived From:    Nothing
+    SystemStatus(const double j1CenterOfGravity, const double j1Weight, const double j1Length,
+                 const double j2CenterOfGravity, const double j2Weight, const double j2Length,
+                 const double j3CenterOfGravity, const double j3Weight, const double j3Length,
+                 const double j4CenterOfGravity, const double j4Weight, const double j4Length,
+                 const double j5CenterOfGravity, const double j5Weight, const double j5Length,
+                 const double j6CenterOfGravity, const double j6Weight, const double j6Length);
+    ~SystemStatus();
+
+    // Description: Calculates the gravity and inertia values of all joints.
+    //              Uses the joint constants in the calculations, so no parameters are necessary.
+    // Arguments:   None
+    // Returns:     Nothing
+    void update();
+
+    // Description: Returns the calculated gravity of the desired joint.
+    // Arguments:   id - the ID of the joint requesting info.
+    // Returns:     the torque gravity of the given joint.
+    double getGravity(uint32_t id);
+
+    // Description: Returns the calculated inertia of the desired joint.
+    // Arguments:   id - the ID of the joint requesting info.
+    // Returns:     the inertia of the given joint.
+    double getInertia(uint32_t id);
+};
+
+#endif /* ROVEJOINTCONTROL_ARMBOARDSOFTWARE_ROVEJOINTCONTROL_SYSTEMSTATUS_H_ */
