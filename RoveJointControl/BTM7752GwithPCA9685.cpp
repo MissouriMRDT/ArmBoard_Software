df0fcab869787936a5ba4bbcc96ca17fdf249417
Drue Satterfield <drscp5@mst.edu>
2018-03-16 01:33:19 -0500

    continued refactoring for new roveboard + cleanup

--- RoveJointControl/BTM7752GwithPCA9685.cpp
+++ RoveJointControl/BTM7752GwithPCA9685.cpp
@@ -10,13 +10,13 @@
 #include "RoveJointUtilities.h"
 
 static bool initialized = false;
-const RoveI2C_Speed DEFAULT_SPEED = I2CSPEED_FAST;
-const uint8_t RESET_ADDRESS = 0b00000000;
-const uint8_t RESET_MSG =     0b000001100;
-const uint8_t M1REGISTER_ADDRESS = 0b00000000;
-const uint8_t M1REGISTER_MSG =     0b00100000; //this will turn off sleep mode, turn on auto increment
-const uint16_t PWM_MAX = 4095;
-const uint8_t MAX_MOTOR_CHANNELS = 7;
+static const RoveI2C_Speed DEFAULT_SPEED = I2CSPEED_FAST;
+static const uint8_t RESET_ADDRESS = 0b00000000;
+static const uint8_t RESET_MSG =     0b000001100;
+static const uint8_t M1REGISTER_ADDRESS = 0b00000000;
+static const uint8_t M1REGISTER_MSG =     0b00100000; //this will turn off sleep mode, turn on auto increment
+static const uint16_t PWM_MAX = 4095;
+static const uint8_t MAX_MOTOR_CHANNELS = 7;
 
 BTM7752GwithPCA9685::BTM7752GwithPCA9685(uint8_t chipAdd, uint8_t motorInd, uint8_t motorEnablePin, uint8_t i2cNenablePin, uint8_t i2cIndex, uint8_t clockPin, uint8_t dataPin, bool inverted)
 : OutputDevice(InputPowerPercent, inverted)
@@ -35,7 +35,7 @@ BTM7752GwithPCA9685::BTM7752GwithPCA9685(uint8_t chipAdd, uint8_t motorInd, uint
   {
     roveI2cSend(i2cHandle, RESET_ADDRESS, RESET_MSG);
     delay(1);
-    roveI2cSend(i2cHandle, chipAddress, M1REGISTER_ADDRESS, M1REGISTER_MSG);
+    roveI2cSendReg(i2cHandle, chipAddress, M1REGISTER_ADDRESS, M1REGISTER_MSG);
     delay(1);
     digitalPinWrite(i2cNenablePin, LOW);
     initialized = true;
@@ -70,17 +70,17 @@ void BTM7752GwithPCA9685::move(const long movement)
     //figure out which channel to send to, and set the opposite channel to 0
     if(mov > 0)
     {
-      roveI2cSendBurst(i2cHandle, chipAddress, channelRegisterBaseOffset+4, offMsg, 4);
+      roveI2cSendBurstReg(i2cHandle, chipAddress, channelRegisterBaseOffset+4, offMsg, 4);
       channelRegisterBaseOffset += 0;
     }
     else if(mov < 0)
     {
-      roveI2cSendBurst(i2cHandle, chipAddress, channelRegisterBaseOffset, offMsg, 4);
+      roveI2cSendBurstReg(i2cHandle, chipAddress, channelRegisterBaseOffset, offMsg, 4);
       channelRegisterBaseOffset += 4;
     }
 
     uint8_t msg[4] = {onRegNum, onRegNum, (uint8_t)offRegNum, ((uint8_t)(offRegNum>>8))};
-    roveI2cSendBurst(i2cHandle, chipAddress, channelRegisterBaseOffset, msg, 4);
+    roveI2cSendBurstReg(i2cHandle, chipAddress, channelRegisterBaseOffset, msg, 4);
   }
   else
   {
@@ -96,20 +96,19 @@ void BTM7752GwithPCA9685::setPower(bool power)
 
 void BTM7752GwithPCA9685::stop()
 {
-      //moving the motor at 0 speed, aka "stopping" it
-      long offRegNum; //the off register determines when the high pulse turns off in pwm
-      long onRegNum = 0; //on register is when high pulse turns on. Make it 0 so it just starts on, then turns off when offRegNum says to
-      uint8_t channelRegisterBaseOffset;
+  //moving the motor at 0 speed, aka "stopping" it
+  long offRegNum; //the off register determines when the high pulse turns off in pwm
+  long onRegNum = 0; //on register is when high pulse turns on. Make it 0 so it just starts on, then turns off when offRegNum says to
+  uint8_t channelRegisterBaseOffset;
 
-      currentMove = 0;
+  currentMove = 0;
 
-      offRegNum = 0;
-      channelRegisterBaseOffset = 6 + 8*motorIndex;
+  offRegNum = 0;
+  channelRegisterBaseOffset = 6 + 8*motorIndex;
 
-      uint8_t msg[4] = {onRegNum, onRegNum, (uint8_t)offRegNum, ((uint8_t)(offRegNum>>8))};
-      roveI2cSendBurst(i2cHandle, chipAddress, channelRegisterBaseOffset, msg, 4);
-      roveI2cSendBurst(i2cHandle, chipAddress, channelRegisterBaseOffset+4, msg, 4); //send to forward and reverse channels
-
+  uint8_t msg[4] = {onRegNum, onRegNum, (uint8_t)offRegNum, ((uint8_t)(offRegNum>>8))};
+  roveI2cSendBurstReg(i2cHandle, chipAddress, channelRegisterBaseOffset, msg, 4);
+  roveI2cSendBurstReg(i2cHandle, chipAddress, channelRegisterBaseOffset+4, msg, 4); //send to forward and reverse channels
 }
 
 long BTM7752GwithPCA9685::getCurrentMove()
--- RoveJointControl/BTM7752GwithPCA9685.h
+++ RoveJointControl/BTM7752GwithPCA9685.h
@@ -11,6 +11,7 @@
 #include "AbstractFramework.h"
 #include "RoveBoard.h"
 
+//class for moving multiple BTM7752G motor controllers with the PCA9685 pwm driver; the latter sends the pwm to the former for us.
 class BTM7752GwithPCA9685: public OutputDevice
 {
   protected:
@@ -36,8 +37,23 @@ class BTM7752GwithPCA9685: public OutputDevice
 
   public:
 
+    /*constructor for when the i2c not-enable pin is used.
+     * inputs:
+     *        chipAdd: the 7 bit chip address for the PCA
+     *        motorInd: the index of which motor this instance of the class is moving, since the pca controls multiple the class
+     *                  needs to know which one an instance is specifically moving. IE 0 for this is motor 0, 1 for motor one, etc.
+     *                  Note: 0 index based.
+     *        motorEnablePin: the pin that enables or disables theb btm
+     *        i2cNenablepin: the pin that enables or disables the pca
+     *        i2cIndex: index of which i2c module is to be used with the i2c pins. See roveboard's i2c.h
+     *        clock pin: i2c clock pin to be used to talk to the pca
+     *        data pin: i2c data pin to be used to talk to the pca
+     *        inverted: Whether or not the motor is 'upside down' IE whether or not to invert move commands in terms of direction.
+    */
     BTM7752GwithPCA9685(uint8_t chipAdd, uint8_t motorInd, uint8_t motorEnablePin, uint8_t i2cNenablePin, uint8_t i2cIndex, uint8_t clockPin, uint8_t dataPin, bool inverted);
     ~BTM7752GwithPCA9685();
+
+    //returns the last movement command the class had
     long getCurrentMove();
 
 };
