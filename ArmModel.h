5c07f05bbd9cd53662f50ace2e0e95a460a18fb2
Eli Verbrugge <EliVerbrugge@users.noreply.github.com>
2019-05-17 18:39:14 -0500

    Mark did things
    And connor watched

--- ArmModel.h
+++ ArmModel.h
@@ -8,7 +8,7 @@ const uint8_t ArmJointCount = 6;
 //D-H Parameters of Arm Model
 const float th1offset=1.57079632679; //should be 90 in order for origin frame to comply with "Rover
 // Coordinate Standard"
-const float d1=2.8937; // height of bicep tilt axis from baseplate/origin
+const float d1=4.1301; // height of bicep tilt axis from baseplate/origin
 const float a1=0; //forward offset of bicep tilt axis relative to base rotate axis
 const float alpha1=1.57079632679; //anglular offset of J1 about X1 axis. (SHOULD BE 90 UNLESS ARM
            // DESIGN IS SUPER FUNKY)
@@ -17,14 +17,14 @@ const float d2=0;//offset to the right of the bicep relative to the base rotatio
      //should probably stay as 0 even if bicep is offset. this offset can
      //also be accounted for using d3)
 const float a2=17;//bicep length(distance between bicep tilt axis and elbow tilt axis)
-const float alpha2=0;//angular offset of elbow tilt axis about x2 axis.(SHOULD BE 90
+const float alpha2=1.57079632679;//angular offset of elbow tilt axis about x2 axis.(SHOULD BE 90
          //UNLESS ARM DESIGN IS SUPER FUNKY)
 const float th3offset=1.57079632679;//should be 90
 const float d3=0;//offset to the right of the forearm relative to the bicep(see d2
      //comment, if the bicep is offset from the base rotate axis but you
      //have d2 as 0, then d3 must be the offset to the right of the forearm
      //relative to the base rotate axis)
-const float a3=2.837;//offset of forearm twist axis from the elbow tilt axis along the x2
+const float a3=0;//offset of forearm twist axis from the elbow tilt axis along the x2
      //axis. (this is the "vertical" offset of the forearm.  DONT USE THIS
      //if you calculated the actual distance between the elbow axis and
      //wrist center and calculated the th3 offset accordingly. in that case
@@ -34,7 +34,7 @@ const float alpha3=1.57079632679;//angular offset of forearm about x3 axis. (SHO
 const float th4offset=0; //angular offset of forearm twist. should be 0 for standard
              //spherical wrist orientation. (phoenix, horison, zenith, and
              //gryphon's wrist joints all complied with this)
-const float d4=17;//Forearm Length. If a3 is zero but there is a "vertical" offset of
+const float d4=17.5;//Forearm Length. If a3 is zero but there is a "vertical" offset of
       //the forearm, this value needs to be the center to center distance
       //between the elbow tilt axis and the wrist center.
 const float a4=0; //needs to be 0 for spherical wrist
